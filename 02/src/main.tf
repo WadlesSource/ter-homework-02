79ed7f963502d3989d1b8bd8f2fb0054b1841c30
WadlesSource <dudunovdimka@gmail.com>
2026-06-08 14:10:23 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -7,35 +7,75 @@ resource "yandex_vpc_subnet" "develop" {
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
-
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "${var.vpc_name}-b"
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.vm_db_cidr
+}
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+ family = var.vm_web_image_family
+
+}
+data "yandex_compute_image" "ubuntu_db" {
+  family = var.vm_db_image_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name        = local.vm_web_computed_name
+  platform_id = var.vm_web_platform_id
+
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vms_resources["web"].cores
+    memory        = var.vms_resources["web"].memory
+    core_fraction = var.vms_resources["web"].core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
-  scheduling_policy {
-    preemptible = true
-  }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    serial-port-enable = var.vms_metadata["common"].serial_port_enable
+    ssh-keys           = "${var.vms_metadata["common"].ssh_user}:${var.vms_ssh_root_key}"
   }
 
+}
+resource "yandex_compute_instance" "platform_db" {
+  name        = local.vm_db_computed_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.vm_db_zone
+
+  resources {
+    cores         = var.vms_resources["db"].cores
+    memory        = var.vms_resources["db"].memory
+    core_fraction = var.vms_resources["db"].core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_db.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_b.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vms_metadata["common"].serial_port_enable
+    ssh-keys           = "${var.vms_metadata["common"].ssh_user}:${var.vms_ssh_root_key}"
+  }
 }
